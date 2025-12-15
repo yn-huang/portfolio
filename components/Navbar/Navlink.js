c85bf2e6a7bd1a1dfdfe2e1ade22a761171667a1
Yinuo Huang <yinuohuang.design@gmail.com>
2025-12-15 12:16:48 -0500

    update react and nextjs packages, update navbar

--- components/Navbar/Navlink.js
+++ components/Navbar/Navlink.js
@@ -4,8 +4,10 @@ import { useState } from "react";
 import Nav from "react-bootstrap/Nav";
 
 import classes from "./Navlink.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Navlink({ children, link }) {
+  const pathname = usePathname();
   const [showHoverLine, setShowHoverLine] = useState(false);
 
   const mouseEnterHandler = () => {
@@ -16,15 +18,17 @@ export default function Navlink({ children, link }) {
     setShowHoverLine(false);
   };
 
+  const isCurrentLink = pathname === link;
+
   return (
     <li className="nav-item d-flex flex-column align-items-start align-items-md-center mx-0 my-3 my-md-0 mx-md-3">
       <Nav.Link
         href={link}
         style={{
           textDecoration: "none",
-          color: "#242323",
+          color: isCurrentLink ? "#96B7DB" : "#242323",
           lineHeight: "initial",
-          padding: 0
+          padding: 0,
         }}
         onMouseEnter={mouseEnterHandler}
         onMouseLeave={mouseLeaveHandler}
@@ -33,7 +37,10 @@ export default function Navlink({ children, link }) {
       </Nav.Link>
       <div
         className={classes["hover-line"]}
-        style={{ backgroundColor: showHoverLine ? "#96B7DB" : "#ffffff" }}
+        style={{
+          backgroundColor:
+            showHoverLine && !isCurrentLink ? "#96B7DB" : "#ffffff",
+        }}
       ></div>
     </li>
   );
